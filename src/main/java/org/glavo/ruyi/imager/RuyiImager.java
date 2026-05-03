ff05889b4c29223fcdd08f88944d989860a63d4e
Glavo <glavo@isrc.iscas.ac.cn>
2026-05-03 15:39:52 +0800

    Add RuyiImager application stub

---
+++ src/main/java/org/glavo/ruyi/imager/RuyiImager.java
@@ -0,0 +1,14 @@
+// Copyright (c) 2026 Glavo
+// SPDX-License-Identifier: MPL-2.0
+
+package org.glavo.ruyi.imager;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public final class RuyiImager extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO
+    }
+}
